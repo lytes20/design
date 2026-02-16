556154907548e5e239d5bb3baa5f0020b50dace3
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2026-02-15 18:33:48 -0600

    Initial commit

---
+++ car-rental/Rent.ts
@@ -0,0 +1,28 @@
+class Address {
+  street: string;
+  city: string;
+  state: string;
+  zip: number;
+}
+
+class Customer {
+  phone: string;
+  email: string;
+  address: Address;
+}
+class RentalRecord {
+  customer: Customer;
+  startDate: Date;
+  maxDuration: number;
+  endDate: Date;
+  car: Car;
+}
+
+class Car {
+  id: string;
+  type: string;
+}
+class Reservation {
+  date: Date;
+  car: Car;
+}
